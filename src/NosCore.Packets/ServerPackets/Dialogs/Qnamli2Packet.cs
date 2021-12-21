47377f323f57a7b0e9f9b14a671bb4b275020993
František Boháček <fandabohacek@gmail.com>
2021-12-21 21:57:30 +0100

    feat: add qnamli2 packet for dialogs

---
+++ src/NosCore.Packets/ServerPackets/Dialogs/Qnamli2Packet.cs
@@ -0,0 +1,27 @@
+﻿// 
+// Qnamli2Packet.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ServerPackets.Dialogs;
+
+[PacketHeader("qnamli2", Scope.InGame)]
+public class Qnamli2Packet : PacketBase
+{
+    [PacketIndex(1)]
+    public string? Command { get; set; }
+
+    [PacketIndex(2)]
+    public Game18NConstString Type { get; set; }
+
+    [PacketIndex(3)]
+    public int ParametersCount { get; set; }
+
+    [PacketListIndex(4, ListSeparator = " ")]
+    public IEnumerable<string>? Parameters { get; set; }
+}
