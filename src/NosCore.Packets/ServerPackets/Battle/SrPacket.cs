af922022785c439fb0df5922058773b2b1c656a5
František Boháček <fandabohacek@gmail.com>
2021-12-26 22:11:43 +0100

    feat: add sr packet

---
+++ src/NosCore.Packets/ServerPackets/Battle/SrPacket.cs
@@ -0,0 +1,17 @@
+﻿// 
+// SrPacket.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ServerPackets.Battle;
+
+[PacketHeader("sr", Scope.InGame)]
+public class SrPacket : PacketBase
+{
+    [PacketIndex(0)]
+    public long SkillVnum { get; set; }
+}
