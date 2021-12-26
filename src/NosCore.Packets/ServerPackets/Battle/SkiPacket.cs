718a7d34438c0f1fd1de55fde5c9d63b7b756a4d
František Boháček <fandabohacek@gmail.com>
2021-12-26 22:11:38 +0100

    feat: add ski packet

---
+++ src/NosCore.Packets/ServerPackets/Battle/SkiPacket.cs
@@ -0,0 +1,25 @@
+﻿// 
+// SkiPacket.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+using NosCore.Packets.Interfaces;
+
+namespace NosCore.Packets.ServerPackets.Battle;
+
+[PacketHeader("ski", Scope.InGame)]
+public class SkiPacket : PacketBase
+{
+    [PacketIndex(0)]
+    public long PrimarySkill { get; set; }
+    
+    [PacketIndex(1)]
+    public long SecondarySkill { get; set; }
+    
+    [PacketListIndex(2, ListSeparator = " ", SpecialSeparator = "|")]
+    public List<SkiSubPacket>? SkiSubPackets { get; set; } 
+}
---
+++ src/NosCore.Packets/ServerPackets/Battle/SkiSubPacket.cs
@@ -0,0 +1,18 @@
+﻿// 
+// SkiSubPacket.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosCore.Packets.Attributes;
+
+namespace NosCore.Packets.ServerPackets.Battle;
+
+public class SkiSubPacket : PacketBase
+{
+    [PacketIndex(0)]
+    public long SkillVNum { get; set; }
+    
+    [PacketIndex(1)]
+    public int Level { get; set; }
+}
